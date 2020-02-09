408db8bef05a59154458a92a92638cd37a49722f
potionk <48196324+potionk@users.noreply.github.com>
2020-02-09 15:38:52 +0900

    Fix bug login function in backend

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser')
+var cors = require('cors');
+
 app.set('port', process.env.PORT || 3001);
 
 const accountRouter = require('./routes/account');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
--- server/routes/account.js
+++ server/routes/account.js
@@ -16,19 +16,19 @@ router.post('/login', async(req, res, next) => {
     const account = req.body.account
     const password = req.body.password
     if(!account) {
-        return res.status(404).send({error: true, code:1})
+        return res.send({error: true, errorCode: 1})
     }
     if(!password) {
-        return res.status(404).send({error: true, code:2})
+        return res.send({error: true, errorCode: 2})
     }
 
     try{
-      const accounts = await accountModel.findOne({account});
+      const accounts = await accountModel.findOne({ where: {name: account}});
       if(!accounts){ // DB에 해당 계정이 없음
-        return res.status(404).send({error: true, code:3})
+        return res.send({error: true, errorCode: 3})
       } else {
         if(accounts.password !== password) {
-          return res.send({error: true, code:4})
+          return res.send({error: true, errorCode: 4})
         } else {
           return res.send({
             account: accounts.name,
@@ -39,7 +39,7 @@ router.post('/login', async(req, res, next) => {
       
     } catch(error) {
       console.error(error);
-      res.send({error: true, code:5})
+      res.send({error: true, errorCode: 5})
     }
 })
 
