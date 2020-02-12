c9b3f640bff4b9cfe94744a01f82733a74cec1cd
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-02-12 15:27:22 +0900

    Add Register function code in register page

--- src/views/Pages/Register/Register.js
+++ src/views/Pages/Register/Register.js
@@ -1,7 +1,82 @@
 import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
 import { Button, Card, CardBody, CardFooter, Col, Container, Form, Input, InputGroup, InputGroupAddon, InputGroupText, Row } from 'reactstrap';
+import axios from "axios";
 
 class Register extends Component {
+  state = {
+    username: '',
+    password: '',
+    repeate_password: '',
+    phone_num: '',
+    email: ''
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // this.props.onCreate(this.state);
+    this.setState({
+      username: '',
+      password: '',
+      repeate_password: '',
+      phone_num: '',
+      email: ''
+    })
+  }
+
+  register = async () => {
+    axios.post("http://localhost:3001/account/register", {
+      username: this.state.username,
+      password: this.state.password,
+      email: this.state.email,
+      phone_num: this.state.phone_num
+    })
+      .then(res => {
+        let data = res.data;
+        console.log(data);
+        if (data.error) {
+          switch (data.errorCode) {
+            case 6:
+              alert("아이디를 입력해주세요.");
+              window.location.reload();
+              return;
+            case 7:
+              alert("비밀번호를 입력해주세요.");
+              window.location.reload();
+              return;
+            case 8:
+              alert("이메일을 입력해주세요.");
+              window.location.reload();
+              return;
+            case 9:
+              alert("전화번호를 입력해주세요.");
+              window.location.reload();
+              return;
+            case 10:
+              alert("존재하는 아이디 입니다.");
+              window.location.reload();
+              return;
+            case 11:
+              alert("DB연결 에러.");
+              window.location.reload();
+              return;
+            default:
+              alert("잘못된 접근입니다.")
+              window.location.reload();
+              return
+          }
+        } else {
+          alert("회원가입이 완료되었습니다.")
+          window.location.replace("/");
+        }
+      }).catch(error => {
+        console.log('failed', error)
+      })
+  }
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -10,7 +85,7 @@ class Register extends Component {
             <Col md="9" lg="7" xl="6">
               <Card className="mx-4">
                 <CardBody className="p-4">
-                  <Form>
+                  <Form onSubmit={this.handleSubmit}>
                     <h1>Register</h1>
                     <p className="text-muted">Create your account</p>
                     <InputGroup className="mb-3">
@@ -19,31 +94,39 @@ class Register extends Component {
                           <i className="icon-user"></i>
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Username" autoComplete="username" />
+                      <Input type="text" value={this.state.username} onChange={this.handleChange} placeholder="Username" autoComplete="username" name="username" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" value={this.state.password} onChange={this.handleChange} placeholder="Password" autoComplete="new-password" name="password" />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                      <InputGroupAddon addonType="prepend">
+                        <InputGroupText>
+                          <i className="icon-lock"></i>
+                        </InputGroupText>
+                      </InputGroupAddon>
+                      <Input type="password" value={this.state.repeate_password} onChange={this.handleChange} placeholder="Repeat password" autoComplete="new-password" name="repeate_password" />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>@</InputGroupText>
                       </InputGroupAddon>
-                      <Input type="text" placeholder="Email" autoComplete="email" />
+                      <Input type="text" value={this.state.email} onChange={this.handleChange} placeholder="Email" autoComplete="email" name="email" />
                     </InputGroup>
                     <InputGroup className="mb-3">
                       <InputGroupAddon addonType="prepend">
                         <InputGroupText>
-                          <i className="icon-lock"></i>
+                          ☏
                         </InputGroupText>
                       </InputGroupAddon>
-                      <Input type="password" placeholder="Password" autoComplete="new-password" />
+                      <Input type="text" value={this.state.phone_num} onChange={this.handleChange} placeholder="Phone_num" autoComplete="phone_num" name="phone_num" />
                     </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon addonType="prepend">
-                        <InputGroupText>
-                          <i className="icon-lock"></i>
-                        </InputGroupText>
-                      </InputGroupAddon>
-                      <Input type="password" placeholder="Repeat password" autoComplete="new-password" />
-                    </InputGroup>
-                    <Button color="success" block>Create Account</Button>
+                    <Button type="submit" color="success" onClick={this.register}>Create Account</Button>
                   </Form>
                 </CardBody>
                 <CardFooter className="p-4">
