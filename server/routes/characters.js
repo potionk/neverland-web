49d71405013435b152aafe44790d6fd8ff8699e5
potionk <48196324+potionk@users.noreply.github.com>
2020-02-21 21:42:45 +0900

    Add get ranking function in back-end

--- server/routes/characters.js
+++ server/routes/characters.js
@@ -2,4 +2,24 @@ var express = require('express');
 var charactersModel = require('../models').characters;
 var router = express.Router();
 
+router.get('/get_ranking', async (req, res, next) => {
+    const characters = await charactersModel.findAll({
+     attributes: ['name', 'level', 'exp', 'job', 'gm', 'createdate'],
+     order: [
+             ['level','DESC'],
+             ['exp','DESC'],
+             ['name', 'ASC'],
+            ]
+    });
+    try {
+        res.send({ characters });
+    } catch (error) {
+        console.log(error);
+        res.send({
+            error:true,
+            errorCode:1
+        });
+    }
+});
+
 module.exports = router;
