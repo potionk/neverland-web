1e8865107cb755177035b16c707adc9768d8c246
potionk <48196324+potionk@users.noreply.github.com>
2020-02-21 19:42:16 +0900

    Add characters skeleton code in back-end

--- server/app.js
+++ server/app.js
@@ -6,12 +6,14 @@ var cors = require('cors');
 app.set('port', process.env.PORT || 3001);
 
 const accountRouter = require('./routes/account');
+const charactersRouter = require('./routes/characters');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.use('/account', accountRouter);
+app.use('/characters', charactersRouter);
 
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
---
+++ server/routes/characters.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var charactersModel = require('../models').characters;
+var router = express.Router();
+
+module.exports = router;
