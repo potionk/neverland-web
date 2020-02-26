cbd98d738ef14b9303245426cbc1983a224ed3af
Miral3 <59829606+Miral3@users.noreply.github.com>
2020-02-26 17:48:47 +0900

    Add view update, post  in front-end

--- src/views/Community/Free/Free.js
+++ src/views/Community/Free/Free.js
@@ -1,9 +1,91 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Badge, Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Row, Table } from 'reactstrap';
+import axios from "axios";
 
-class Free extends Component {
+import FreeData from './FreeData'
+
+function UserRow(props) {
+  const user = props.user
+  const userLink = `/users/${user.id}`
+
+  const getBadge = (status) => {
+    return status === 'Active' ? 'success' :
+      status === 'Inactive' ? 'secondary' :
+        status === 'Pending' ? 'warning' :
+          status === 'Banned' ? 'danger' :
+            'primary'
+  }
+
+  return (
+    <tr key={user.id.toString()}>
+      <td>{user.id}</td>
+      <td><Link to={userLink}>{user.title}</Link></td>
+      <td>{user.views}</td>
+      <td><Link to={userLink}>{user.name}</Link></td>
+      <td>{user.registered}</td>
+    </tr>
+  )
+}
+
+class Users extends Component {
+
   render() {
-    return <div>Free</div>;
+    const userList = FreeData.filter((user) => user.id < 10)
+    // const userList = this.state.usersData;
+
+    return (
+      <div className="animated fadeIn">
+        <Row>
+          <Col xl="12">
+            <Card>
+              <CardHeader>
+                <i className="fa fa-align-justify"></i> Post <small className="text-muted">Free</small>
+              </CardHeader>
+              <CardBody>
+                <Table responsive hover>
+                  <thead>
+                    <tr>
+                      <th scope="col">Num</th>
+                      <th scope="col">Title</th>
+                      <th scope="col">Views</th>
+                      <th scope="col">Author</th>
+                      <th scope="col">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {userList.map((user, index) =>
+                      <UserRow key={index} user={user} />
+                    )}
+                  </tbody>
+                </Table>
+                <Pagination className="d-flex justify-content-center">
+                  <PaginationItem>
+                    <PaginationLink previous tag="button"></PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem active>
+                    <PaginationLink tag="button">1</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">2</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">3</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink tag="button">4</PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink next tag="button"></PaginationLink>
+                  </PaginationItem>
+                </Pagination>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+      </div>
+    )
   }
 }
 
-export default Free;
+export default Users;
---
+++ src/views/Community/Free/FreeData.js
@@ -0,0 +1,32 @@
+const FreeData = [
+    {id: 0, title: '이용하 김종운 열애설', name: 'Agapetus Tadeáš',views: '9999',registered: '2020/02/25', role: 'Guest', status: 'Pending'},
+    {id: 1, title: '메이플은 질병이다', name: 'Agapetus Tadeáš',views: '10800',registered: '2018/01/01', role: 'Member', status: 'Active'},
+    {id: 2, title: '탈메는 지능순', name: 'Agapetus Tadeáš',views: '1183',registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+    {id: 3, title: '[속보]인성은 DB 성적에 비례해...', name: 'Agapetus Tadeáš',views: '1931',registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
+    {id: 4, title: 'Miral', name: 'Agapetus Tadeáš',views: '1231',registered: '2018/03/01', role: 'Member', status: 'Pending'},
+    {id: 5, title: 'Potionk', name: 'Agapetus Tadeáš',views: '4124',registered: '2018/01/21', role: 'Staff', status: 'Active'},
+    {id: 6, title: '똑똑', name: 'Agapetus Tadeáš',views: '1514',registered: '2018/01/01', role: 'Member', status: 'Active'},
+    {id: 7, title: 'test', name: 'Agapetus Tadeáš',views: '12415',registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+    {id: 8, title: '흐응~', name: 'Agapetus Tadeáš',views: '215125',registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
+    {id: 9, title: '이잉~기모링~', name: 'Agapetus Tadeáš',views: '1251', registered: '2018/03/01', role: 'Member', status: 'Pending'},
+    {id: 10, name: 'Agapetus Tadeáš', registered: '2018/01/21', role: 'Staff', status: 'Active'},
+    {id: 11, name: 'Carwyn Fachtna', registered: '2018/01/01', role: 'Member', status: 'Active'},
+    {id: 12, name: 'Nehemiah Tatius', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+    {id: 13, name: 'Ebbe Gemariah', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
+    {id: 14, name: 'Eustorgios Amulius', registered: '2018/03/01', role: 'Member', status: 'Pending'},
+    {id: 15, name: 'Leopold Gáspár', registered: '2018/01/21', role: 'Staff', status: 'Active'},
+    {id: 16, name: 'Pompeius René', registered: '2018/01/01', role: 'Member', status: 'Active'},
+    {id: 17, name: 'Paĉjo Jadon', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+    {id: 18, name: 'Micheal Mercurius', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
+    {id: 19, name: 'Ganesha Dubhghall', registered: '2018/03/01', role: 'Member', status: 'Pending'},
+    {id: 20, name: 'Hiroto Šimun', registered: '2018/01/21', role: 'Staff', status: 'Active'},
+    {id: 21, name: 'Vishnu Serghei', registered: '2018/01/01', role: 'Member', status: 'Active'},
+    {id: 22, name: 'Zbyněk Phoibos', registered: '2018/02/01', role: 'Staff', status: 'Banned'},
+    {id: 23, name: 'Einar Randall', registered: '2018/02/01', role: 'Admin', status: 'Inactive'},
+    {id: 24, name: 'Félix Troels', registered: '2018/03/21', role: 'Staff', status: 'Active'},
+    {id: 25, name: 'Aulus Agmundr', registered: '2018/01/01', role: 'Member', status: 'Pending'},
+    {id: 42, name: 'Ford Prefex', registered: '2001/05/21', role: 'Alien', status: 'Don\'t panic!'}
+  ]
+  
+  export default FreeData
+  
